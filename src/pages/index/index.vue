32a210248ad5d86774c89f17bf4b3cff2f484907
Ross Cournoyer <rcoupvc@yahoo.com>
2020-04-24 14:36:32 +0800

    Remove extra files

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,126 +1,129 @@
 <template>
-  <div @click="clickHandle">
-
-    <div class="userinfo" @click="bindViewTap">
-      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover" />
-      <img class="userinfo-avatar" src="/static/images/user.png" background-size="cover" />
-
-      <div class="userinfo-nickname">
-        <card :text="userInfo.nickName"></card>
-      </div>
-    </div>
-
-    <div class="usermotto">
-      <div class="user-motto">
-        <card :text="motto"></card>
-      </div>
-    </div>
-
-    <form class="form-container">
-      <input type="text" class="form-control" :value="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model="motto" placeholder="v-model" />
-      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
-    </form>
-
-    <a href="/pages/logs/main" class="counter">去往Vuex示例页面</a>
-
-    <div class="all">
-        <div class="left">
-        </div>
-        <div class="right">
-        </div>
-    </div>
+  <div class="page">
+   <map id="map" 
+   class="page__map"
+   :scale="scale"
+   :latitude="latitude"
+   :longitude="longitude"
+   enable-zoom="false"
+   enable-rotate="false"
+   :markers="markers"
+   @markertap="markerTap"
+   ></map>
+   <div v-if="storeDisplay" class="page__store-info">
+     <h2 class="page__store-info-title">{{ storeDisplay.name }}</h2>
+     <p class="page__store-info-field">Address: {{ storeDisplay.address }}</p>
+     <p class="page__store-info-field">Hours: {{ storeDisplay.hours }}</p>
+     <p class="page__store-info-field">Phone: {{ storeDisplay.phone }}</p>
+   </div>
   </div>
 </template>
 
 <script>
-import card from '@/components/card'
+// Use Vuex
+import store from './store'
 
 export default {
-  data () {
+  mounted() {
+    this.mapCtx = wx.createMapContext('map');
+    this.mapCtx.includePoints({
+      padding: [30],
+      points: [{
+        latitude: 31.1686,
+        longitude: 121.5265,
+      }, {
+        latitude: 31.1128,
+        longitude: 121.4817,
+      },
+      {
+        latitude: 31.235556,
+        longitude: 121.607935,
+      }
+      ]
+    });
+    this.mapCtx.getRegion({
+      success(sw, ne) {
+        console.log(sw, ne)
+      }
+    })
+  },
+  data() {
     return {
-      motto: 'Hello miniprograme',
-      userInfo: {
-        nickName: 'mpvue',
-        avatarUrl: 'http://mpvue.com/assets/logo.png'
-      }
+      scale:"",
+      latitude: "",
+      longitude: "",
+      storeDisplay: null,
+      markers: [
+        {
+          iconPath: "/static/images/mapPin.png",
+          id: 0,
+          data: {name: 'Xuhui Store', address: '165 Changshu Lu', hours: 'Tue-Sat 10AM-6PM', phone: '1281791514'},
+          latitude: 31.1686,
+          longitude: 121.5265,
+          width: 20,
+          height: 20,
+          markerTap: this.markerTap
+        },
+        {
+          iconPath: "/static/images/mapPin.png",
+          id: 1,
+          data: {name: 'Minhang Store', address: '66 Xianfeng Lu', hours: 'Mon-Fri 9AM-5PM', phone: '13817915142'},
+          latitude: 31.1128,
+          longitude: 121.4817,
+          width: 20,
+          height: 20
+        },
+        {
+          iconPath: "/static/images/mapPin.png",
+          id: 2,
+          data: {name: 'Pudong Store', address: '501 Yincheng Middle Rd', hours: 'Mon-Fri 9AM-5PM', phone: '11827915142'},
+          latitude: 31.235556,
+          longitude: 121.607935,
+          width: 20,
+          height: 20
+      }],
     }
   },
-
-  components: {
-    card
-  },
-
   methods: {
-    bindViewTap () {
-      const url = '../logs/main'
-      if (mpvuePlatform === 'wx') {
-        mpvue.switchTab({ url })
-      } else {
-        mpvue.navigateTo({ url })
-      }
-    },
-    clickHandle (ev) {
-      console.log('clickHandle:', ev)
-      // throw {message: 'custom test'}
+    markerTap(e) {
+      const tappedMarker = this.markers.filter(marker => (e.mp.markerId === marker.id))[0];
+      const { latitude, longitude } = tappedMarker;
+      const { name, address, hours, phone } = tappedMarker.data;
+      this.storeDisplay = { name, address, hours, phone };
+      const coords = { latitude, longitude }
+      this.latitude = latitude;
+      this.longitude = longitude;
     }
-  },
-
-  created () {
-    // let app = getApp()
   }
 }
 </script>
 
-<style scoped>
-.userinfo {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+<style>
+.page {
+  text-align: center;
+  margin-top: 20px;
 }
-
-.userinfo-avatar {
-  width: 128rpx;
-  height: 128rpx;
-  margin: 20rpx;
-  border-radius: 50%;
+.page__map {
+  top: 0px;
+  height: 30vh;
+  width: 100%;
 }
-
-.userinfo-nickname {
-  color: #aaa;
+.page__store-info {
+  margin-top: 30px;
 }
-
-.usermotto {
-  margin-top: 150px;
+.page__store-info-title {
+  margin-bottom: 10px;
+  font-size: 25px;
 }
-
-.form-control {
-  display: block;
-  padding: 0 12px;
-  margin-bottom: 5px;
-  border: 1px solid #ccc;
+.page__store-info-field {
+  margin-bottom: 5rpx;
+  font-size: 18px;
 }
-.all{
-  width:7.5rem;
-  height:1rem;
-  background-color:blue;
-}
-.all:after{
-  display:block;
-  content:'';
-  clear:both;
-}
-.left{
-  float:left;
-  width:3rem;
-  height:1rem;
-  background-color:red;
-}
-
-.right{
-  float:left;
-  width:4.5rem;
-  height:1rem;
-  background-color:green;
+.home {
+  display: inline-block;
+  margin: 100px auto;
+  padding: 5px 10px;
+  color: blue;
+  border: 1px solid blue;
 }
 </style>
